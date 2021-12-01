0bddfdf40a2fe7f87012442e244e4f27a9a827b9
vanessaP <vaneedh@gmail.com>
2021-12-01 13:27:46 -0500

    se realizaron cambios en el archivo itemlistcontainer añadiendo constantes y una promesa

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,11 +1,29 @@
-import ItemCount from "./ItemCount"
+import {useEffect, useState} from "react"
+/*import ItemCount from "./ItemCount"*/
+import ItemList from "./ItemList"
+import { getProducts } from "./products"
 
-function ItemListContainer({greeting}){
+
+const ItemListContainer = () => {
+    const[products, setProducts] = useState([])
+
+    useEffect(() => {
+        const list = getProducts()
+
+        list.then((list) => {
+            setProducts(list)
+        })
+    })
+    return(
+            <ItemList product={products}/>
+    )
+}
+
+/*function ItemListContainer({greeting}){
     return<>
     <h1>{greeting}</h1>
     <ItemCount stock={20} initial={1} onAdd={"Se añadio a la canasta"} />
     </>
-}
-
+}*/
 
 export default ItemListContainer
